89bf04ab4ea432e264ae4113d803c7c9917a1d80
minjin kim <sajajin115@gmail.com>
2024-05-26 14:24:01 +0900

    public file 만듬

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ const app = express()
 
 const home = require("./src/routes/home")
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`));
 
 app.set("views","./src/views");
 app.set("view engine","ejs");

5146ba292325630a74c3fba3781553afd0ab724f
minjin kim <sajajin115@gmail.com>
2024-05-26 14:09:34 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const app = express()
 
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 app.use("/", home);
 
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
 
 module.exports = app;
